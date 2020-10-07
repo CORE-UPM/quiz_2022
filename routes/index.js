7732a7d1039bc382e4f18b577de1e72bb044d960
Santiago Pavon <santiago.pavon@upm.es>
2020-10-07 11:58:57 +0200

    Step 1.11: Restoration Routes.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+//-----------------------------------------------------------
+
+// History: Restoration routes.
+
+// Redirection to the saved restoration route.
+function redirectBack(req, res, next) {
+  const url = req.session.backURL || "/";
+  delete req.session.backURL;
+  res.redirect(url);
+}
+
+router.get('/goback', redirectBack);
+
+// Save the route that will be the current restoration route.
+function saveBack(req, res, next) {
+  req.session.backURL = req.url;
+  next();
+}
+
+// Restoration routes are GET routes that do not end in:
+//   /new, /edit, /play, /check, or /:id.
+router.get(
+    [
+      '/',
+      '/author'
+    ],
+    saveBack);
+
+//-----------------------------------------------------------
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
